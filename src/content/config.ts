5b3168b4de6b26a7940adf899bcdda04c7c252c6
midudev <miduga@gmail.com>
2025-12-26 16:15:18 +0100

    Add authors collection schema to config for enhanced author data management in tips

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
+const authors = defineCollection({
+	type: 'data',
+	schema: z.object({
+		name: z.string(),
+		initials: z.string(),
+		color: z.string().optional(),
+		photo: z.string().optional(),
+		bio: z.string(),
+		x: z.string().optional(),
+		github: z.string().optional(),
+	}),
+});
+
 const tips = defineCollection({
 	type: 'content',
 	schema: z.object({
@@ -8,9 +21,7 @@ const tips = defineCollection({
 		category: z.string(),
 		categoryColor: z.string().optional(),
 		author: z.string(),
-		authorInitials: z.string(),
-		authorColor: z.string().optional(),
 	}),
 });
 
-export const collections = { tips };
+export const collections = { tips, authors };
